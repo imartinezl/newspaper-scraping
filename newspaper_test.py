01210f034a8b2f33095f6386b157583457abd7d7
imartinezl <inigomlap@gmail.com>
2019-02-12 22:13:13 +0100

    Source by category

--- newspaper_test.py
+++ newspaper_test.py
@@ -1,30 +1,38 @@
 import newspaper
+import json
 
 data = []
-url = 'https://www.diariovasco.com/'
-dv_paper = newspaper.Source(url, language='es', request_timeout=10, number_threads=10, memoize_articles=False, keep_article_html=False)
-dv_paper.build()
-print(dv_paper.size())
-for article in dv_paper.articles[1720:1750]:
-    print(article.url)
-    # url = 'https://www.diariovasco.com/economia/macroparque-eolico-siemens-asteasu-20190210001331-ntvo.html'
-    # article = newspaper.Article(url)
-    # Try-Except-Continue will skip to the next article in the For loop if there is an exception
-    try:
-        article.download()
-    except ArticleException:
-        continue
-    article.parse()
 
-    item = {
-    'url': article.url, 
-    'author': article.authors,
-    'title': article.title,
-    'top_image': article.top_image,
-    'text': article.text,
-    'date': article.publish_date
-    }
-    data.append(item)
+with open('categories_b.json') as json_categories:           
+    categories = json.load(json_categories)
+
+
+categories = ['https://www.diariovasco.com/']
+for category in categories:
+    dv_cat = newspaper.Source(category, language='es', request_timeout=10, number_threads=10, memoize_articles=False, keep_article_html=False)
+    dv_cat.build()
+    print(dv_cat.size())
+    for article in dv_cat.articles:
+        if((article.url.startswith(category)) and ("#" not in article.url)):
+            print(article.url)
+            # Try-Except-Continue will skip to the next article in the For loop if there is an exception
+            try:
+                article.download()
+                article.parse()
+            except:
+                print('***FAILED TO DOWNLOAD***', article.url)
+                continue
+
+            print(article.url)
+            item = {
+            'date': article.publish_date,
+            'url': article.url, 
+            'author': article.authors,
+            'title': article.title,
+            'top_image': article.top_image,
+            'text': article.text
+            }
+            data.append(item)
 
 import csv
 csv_columns = ['url','author','title','top_image','text','date']
