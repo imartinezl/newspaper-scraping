0b058d7d6129d53cc07794080346960dd2e80587
imartinezl <inigomlap@gmail.com>
2019-02-15 17:46:42 +0100

    News index and article downloading and parsing

--- newspaper_feedparser.py
+++ newspaper_feedparser.py
@@ -7,15 +7,10 @@ import html
 import newspaper
 for i in range(len(d.entries)):
 	d.entries[i].title =  html.unescape(d.entries[i].title)
-	# article = newspaper.Article(d.entries[i].link);
-	# try:
-	#     article.download()
-	#     article.parse()
-	# except:
-	#     print('***FAILED TO DOWNLOAD***', article.url)
-	#     del d.entries[i]
-	#     continue
-	# d.entries[i].text = article.text
+	d.entries[i].summary =  html.unescape(d.entries[i].summary)
+	d.entries[i]['downloaded'] = False
+	d.entries[i]['index'] = i
+
 
 from flask import Flask
 from flask import render_template
@@ -30,9 +25,28 @@ def base():
 def index():
     return render_template('index.html', title='DV', entries=d.entries)
 
-@app.route('/index/<t>')
-def index_title(t):
-    return render_template('item.html', title='DV', t=t)
+@app.route('/index/<entry_index>')
+def article(entry_index):
+	j = int(entry_index)
+	entry = d.entries[j]
+
+	if not d.entries[j].downloaded:
+		article = newspaper.Article(d.entries[j].link)
+		print('***DOWNLOADING***', article.url)
+		try:
+			article.download()
+			article.parse()
+		except:
+			print('***FAILED TO DOWNLOAD***', article.url)
+			# del d.entries[j]
+			return 0
+		d.entries[j].top_image = article.top_image
+		d.entries[j].text = article.text
+		print(article.text)
+		d.entries[j]['downloaded'] = True
+	return render_template('article.html', title='DV', entry=d.entries[j])
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
