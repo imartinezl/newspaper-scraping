ed534cc5d71f33328512e5c3c8019a4315ae9bde
imartinezl <inigomlap@gmail.com>
2020-05-21 20:57:09 +0200

    md5 hash routing + new img source

--- newspaper_feedparser.py
+++ newspaper_feedparser.py
@@ -5,15 +5,42 @@ import newspaper
 import json
 import flask
 from flask_session import Session
+from lxml.html import fromstring
+import requests
+import hashlib
+
+
+m = hashlib.md5()
+def str2md5(text):
+	m.update(text.encode())
+	num = str(int(m.hexdigest(), 16))
+	return num
 
 def rssEntries(rss_url):
-	d = feedparser.parse(rss_url);
+	print(rss_url)
+	web_page = requests.get(rss_url)
+	d = feedparser.parse(web_page.content) # rss_url
 
 	for i in range(len(d.entries)):
-		d.entries[i].title =  html.unescape(d.entries[i].title);
-		d.entries[i].summary =  html.unescape(d.entries[i].summary);
-		d.entries[i]['downloaded'] = False;
-		d.entries[i]['index'] = i;
+		# Unescape title and summary
+		d.entries[i].title =  html.unescape(d.entries[i].title)
+		d.entries[i].summary =  html.unescape(d.entries[i].summary)
+
+		# Generate md5 ID from title
+		d.entries[i]['index'] = str2md5(d.entries[i].title)
+
+		# Feature to cache downloaded articles
+		d.entries[i]['downloaded'] = False
+
+		# Extract text and img from summary
+		summary = d.entries[i].summary
+		if len(summary) > 0:
+			doc = fromstring(summary)
+			img_src = doc.xpath('//img/@src')
+			if img_src:
+				img_src = img_src[-1]
+			d.entries[i]['imagen'] = img_src
+			d.entries[i].summary = doc.text_content()
 	return d.entries
 
 # def getAllRss(rss_array):
@@ -38,51 +65,56 @@ Session(app)
 def base():
     return flask.render_template('base.html', rss_array=rss_array)
 
-@app.route('/<rss_id>')
-def section(rss_id):
-    try:
-        rss_id = int(rss_id)
-    except:
-        return
-    entries = rssEntries(rss_array[rss_id]['url'])
-    flask.session["entries"] = entries;
-    return flask.render_template('section.html', rss_array=rss_array, rss_id = rss_id, entries=entries)
+@app.route('/<rss_title>')
+def section(rss_title):
+	rss_id = 0
+	for i in range(len(rss_array)):
+		if rss_array[i]['title'] == rss_title:
+			rss_id = i
+			break
+	entries = rssEntries(rss_array[rss_id]['url'])
+	flask.session["entries"] = entries
+	return flask.render_template('section.html', rss_title=rss_title, rss_array = rss_array, entries=entries)
 
 
-@app.route('/<rss_id>/<entry_index>')
-def article(rss_id, entry_index):
-    try:
-        rss_id = int(rss_id)
-        i = int(entry_index)
-    except:
-        return 0
+@app.route('/<rss_title>/<entry_index>')
+def article(rss_title, entry_index):
+	entries = flask.session.get("entries")
 
-    entries = flask.session.get("entries")
+	entry_id = 0
+	for i in range(len(entries)):
+		if entries[i]['index'] == entry_index:
+			entry_id = i
+			break
+	print("entry id =>", entry_id)
+	# Issue with some rss links
+	url = entries[entry_id].link
+	if url.count('http') > 1 :
+		entries[entry_id].link = url[url.rfind('http'):]
 
-    # Issue with some rss links
-    url = entries[i].link
-    if url.count('http') > 1 :
-        entries[i].link = url[url.rfind('http'):]
+	if not entries[entry_id].downloaded:
+		web_page = requests.get(entries[entry_id].link)
+		article = newspaper.Article(entries[entry_id].link, verbose=True)
+		print(article)
+		print('***DOWNLOADING***', entries[entry_id].link)
+		try:
+			article.download()
+			print('***DOWNLOADED***', article.download_state)
+			article.parse()
+		except:
+			print('***FAILED TO DOWNLOAD***', entries[entry_id].link)
+			return flask.render_template('base.html', rss_array=rss_array)
+			# del rss_data[rss_id].entries[i]
+			# return 0
+		entries[entry_id].top_image = article.top_image
+		entries[entry_id].html = article.html
+		entries[entry_id].text = article.text
+		entries[entry_id]['downloaded'] = True
+	else:
+		print('***ALREADY DOWNLOADED***', entries[entry_id].link)
+	flask.session["entries"] = entries
 
-    if not entries[i].downloaded:
-        article = newspaper.Article(entries[i].link)
-        print('***DOWNLOADING***', entries[i].link)
-        try:
-        	article.download()
-        	article.parse()
-        except:
-        	print('***FAILED TO DOWNLOAD***', entries[i].link)
-        	# del rss_data[rss_id].entries[i]
-        	return 0
-        entries[i].top_image = article.top_image
-        entries[i].html = article.html
-        entries[i].text = article.text
-        entries[i]['downloaded'] = True
-    else:
-        print('***ALREADY DOWNLOADED***', entries[i].link)
-    flask.session["entries"] = entries;
-
-    return flask.render_template('article.html', rss_array=rss_array, entry=entries[i])
+	return flask.render_template('article.html', rss_array=rss_array, entry=entries[entry_id])
 
 
 if __name__ == '__main__':
